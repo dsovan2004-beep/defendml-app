9daf6e2b3f3c5cebb86ff2468b43f72e722a84e3
dsovan2004-beep <dsovan2004@gmail.com>
2025-10-10 00:42:36 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export' // static export for Cloudflare Pages
+  output: 'export',
+  images: { unoptimized: true } // keeps next/image happy on static export
 };
+
 module.exports = nextConfig;
