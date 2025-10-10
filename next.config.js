33f924327cdcdfbf08b145bf223e60d7eb2c3e47
dsovan2004-beep <dsovan2004@gmail.com>
2025-10-10 00:44:19 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true } // keeps next/image happy on static export
+  images: { unoptimized: true }
 };
-
 module.exports = nextConfig;
